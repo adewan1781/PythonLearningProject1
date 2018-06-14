72d7c53c8b726e053d3c53c9e95539189efa569e
adewan1781 <adewan1781@gmail.com>
2018-06-14 18:25:27 +0530

    simple login logout

--- com/box/tests/BoxLogin.py
+++ com/box/tests/BoxLogin.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,7 +18,20 @@ class BoxLogin:
         assert "Box | Login" in driver.title, "Title is incorrect"
         # driver.find_element_by_name("login")
         element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
-        element.send_keys ("Test Profile1")
+        element.send_keys ("nancydhingra131@gmail.com")
+        element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type=\"submit\"]")))
+        element.click()
+        element = WebDriverWait (driver, 20).until(EC.presence_of_element_located ((By.NAME, "password")))
+        element.send_keys ("nancy131")
+        element = WebDriverWait (driver, 20).until(EC.presence_of_element_located ((By.CSS_SELECTOR, "button[type=\"submit\"]")))
+        element.click()
+        assert "All Files | Powered By Box" in driver.title, "Title is incorrect"
+        element = WebDriverWait (driver, 20).until(EC.presence_of_element_located ((By.CSS_SELECTOR, "button[data-resin-target=\"accountmenu\"]")))
+        element.click()
+        element = WebDriverWait (driver, 20).until(EC.presence_of_element_located ((By.CSS_SELECTOR, "a[data-resin-target=\"logout\"]")))
+        element.click()
+        time.sleep(5)
+
         driver.quit()
 
 if __name__ == '__main__':
