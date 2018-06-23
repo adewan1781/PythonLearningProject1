993ee4bfc4306a43803e705beffb5213fc17e6e6
adewan1781 <adewan1781@gmail.com>
2018-06-23 15:55:14 +0530

    waitAndFindElement method corrected

--- com/box/com/box/utilities/BoxUtilities.py
+++ com/box/com/box/utilities/BoxUtilities.py
@@ -1,6 +1,5 @@
 import time
 
-import nil as nil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,40 +20,33 @@ class BoxUtilities:
 
     def loginProcess(self, username, password):
 
-        element = self.waitAndFindElement('name', 'login')
-        # element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
-        element.send_keys(username)
-        element = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[type=\"submit\"]")))
-        element.click()
-        element = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.NAME, "password")))
-        element.send_keys(password)
-        element = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[type=\"submit\"]")))
-        element.click()
+        element = self.waitAndFindElement(By.NAME, "login")
 
-        # assert "All Files | Powered By Box" in driver.title, "Title is incorrect"
+        element.send_keys(username)
+        element = self.waitAndFindElement(By.CSS_SELECTOR, "button[type=\"submit\"]")
+        element.click()
+        element = self.waitAndFindElement (By.NAME, "password")
+        element.send_keys(password)
+        element = self.waitAndFindElement (By.CSS_SELECTOR, "button[type=\"submit\"]")
+        element.click()
 
     def logOutProcess(self):
-        element = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-resin-target=\"accountmenu\"]")))
+        element = self.waitAndFindElement (By.CSS_SELECTOR, "button[data-resin-target=\"accountmenu\"]")
         element.click()
-        element = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "a[data-resin-target=\"logout\"]")))
+        element = self.waitAndFindElement (By.CSS_SELECTOR, "a[data-resin-target=\"logout\"]")
+
         element.click()
         time.sleep(5)
 
     def driverQuit(self):
         self.driver.quit()
-
+    #
     def waitAndFindElement(self, byLocator, value):
-        print(value, byLocator)
-        element = nil
+        print("Web Element with ",byLocator,":", value)
+        element = None
         try:
-            if byLocator == 'name':
-                element = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(By.NAME, value))
+            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((byLocator, value)))
         except Exception:
+            print ("Web Element with ",byLocator,":", value, " not found")
             self.driver.save_screenshot("/Users/rajat/pythonworkspace/PythonLearningProject1/screenshot.png")
-        finally:
-            print("Web Element with value: ", value, " not found")
         return element
