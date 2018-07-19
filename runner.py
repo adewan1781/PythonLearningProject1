bab8574d960b515c20b0d3dae35f337fe2013b72
Rajat Kaushal <rk@Rajats-MacBook-Pro.local>
2018-07-19 18:57:28 +0530

    small change

--- runner.py
+++ runner.py
@@ -5,7 +5,5 @@ import os
 
 currentPath = os.getcwd()
 print(currentPath)
-cmd = "cd com\\behave && behave -t @add,@sub,@div,@mul --junit"
+cmd = "cd com//behave && behave -t @add,@sub,@div,@mul --junit"
 os.system(cmd)
-
-
